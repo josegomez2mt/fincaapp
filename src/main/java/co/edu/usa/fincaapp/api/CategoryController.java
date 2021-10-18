e96b14d366b5278a63a8dd3771931452eb5660b1
José Pascual Gómez Blanco <josepascualgomez@gmail.com>
2021-10-18 09:59:52 -0500

    Ajuste del delete
    Solo en categoria

--- src/main/java/co/edu/usa/fincaapp/api/CategoryController.java
+++ src/main/java/co/edu/usa/fincaapp/api/CategoryController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +23,7 @@ import co.edu.usa.fincaapp.servicios.CategoryService;
 
 @RestController
 @RequestMapping("api/Category")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET,RequestMethod.POST, RequestMethod.DELETE })
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
@@ -44,8 +46,9 @@ public class CategoryController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Long> deletePost(@PathVariable Long id) {
-        categoryService.delete(id);
-        return new ResponseEntity<>(HttpStatus.OK);
+    @ResponseStatus(code = HttpStatus.OK)
+    public void deletePost(@PathVariable Long id) {
+        categoryService.delete(id);        
     }
+    
 }
